fe47c60a83583668acd25e25bb6402861b9d45ad
Karol Cedro <karol-cedro@wp.pl>
2021-03-14 20:41:17 +0100

    Fixing a database structure

--- db_create.sql
+++ db_create.sql
@@ -25,11 +25,17 @@ ALTER TABLE mt_playlists ADD CONSTRAINT play_user_fk FOREIGN KEY (user_id) REFER
 
 CREATE TABLE mt_songs(
     song_id SERIAL PRIMARY KEY NOT NULL,
-    playlist_id INTEGER DEFAULT NULL,
     user_id INTEGER NOT NULL,
     artist VARCHAR(30) NOT NULL,
     title VARCHAR(50) NOT NULL
 );
 
-ALTER TABLE mt_songs ADD CONSTRAINT song_playlist_fk FOREIGN KEY (playlist_id) REFERENCES mt_playlists(playlist_id);
 ALTER TABLE mt_songs ADD CONSTRAINT song_user_fk FOREIGN KEY (user_id) REFERENCES mt_users(user_id);
+
+CREATE TABLE mt_playlist_song(
+    song_id INTEGER  NOT NULL,
+    playlist_id INTEGER  NOT NULL
+);
+
+ALTER TABLE mt_playlist_song ADD CONSTRAINT playlist_fk FOREIGN KEY (playlist_id) REFERENCES mt_playlists(playlist_id);
+ALTER TABLE mt_playlist_song ADD CONSTRAINT song_fk FOREIGN KEY (song_id) REFERENCES mt_songs(song_id);
